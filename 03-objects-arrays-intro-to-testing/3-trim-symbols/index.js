1c7cae476782a235ac06962d630e18987c9b43aa
Deepgmc <deepgmc_job@mail.ru>
2020-08-29 14:13:15 +0300

    trim symbols solution. предварительно, т.к. какой-то код корявый, не красивый

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  let last_char = '',
+      counter   = 1
+  return [...string].filter((char) => {
+      let result
+      if (
+          last_char !== char && counter <= size) {
+          counter = 1
+          last_char = char
+          result = char
+      } else {
+          if (counter >= size) {
+            result = false
+          } else {
+              counter++
+              result = char
+          }
+      }
+      return result
+    })
+    .join('')
 }
