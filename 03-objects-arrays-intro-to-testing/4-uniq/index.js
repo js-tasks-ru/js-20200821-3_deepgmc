658de467cbb32f85a418a8dfa66202bce901256d
Deepgmc <deepgmc_job@mail.ru>
2020-08-29 13:47:16 +0300

    uniq solution

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,10 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
-
+export function uniq(arr = []) {
+  // для прохождения теста на "пустой аргумент" - добавил параметр по умолчанию,
+  // но вроде же исходный код менять нельзя?
+  return arr.filter((val, index) => {
+    return arr.indexOf(val) === index;
+  })
 }
