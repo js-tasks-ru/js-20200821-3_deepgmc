aed4bf111fb363bc8a16b334c00f3cb419d6ce10
Deepgmc <deepgmc_job@mail.ru>
2020-09-04 08:46:28 +0300

    working version

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,77 +1,79 @@
 export default class ColumnChart {
     element
+    graphValue = 'def'
+    graphLabel = 'def'
+    graphHref = 'def'
+    chartHeight = 50 // высота в пикселях из цсс
     
-    constructor({
-                    data = [],
-                    label = '',
-                    href = '',
-                    value = null
-                }) {
+    constructor({ data = [], label = '', href = '', value = null }) {
         this.data = data
-        this.label = label
-        this.href = href
-        this.value = value
+        this.graphHref = href
+        this.graphValue = value
+        this.graphLabel = label
         this.render()
     }
     
-    render() {
-        const wrap_element = document.createElement('div')
-        wrap_element.innerHTML = this.tpl
-        this.element = wrap_element.firstElementChild
-    }
-    
-    tpl = `<div class="column-chart">
-              <div class="column-chart__title">
-                Total orders
-                ${this.link}
-              </div>
-              <div class="column-chart__container">
-                <div class="column-chart__header">344</div>
-                <div class="column-chart__chart">
-                  <div style="--value:28" data-tooltip="<div><small>15 янв. 2020 г.</small></div><strong>11</strong>"></div>
-                  <div style="--value:28" data-tooltip="<div><small>16 янв. 2020 г.</small></div><strong>11</strong>"></div>
-                  <div style="--value:23" data-tooltip="<div><small>17 янв. 2020 г.</small></div><strong>9</strong>"></div>
-                  <div style="--value:7" data-tooltip="<div><small>18 янв. 2020 г.</small></div><strong>3</strong>"></div>
-                  <div style="--value:34" data-tooltip="<div><small>19 янв. 2020 г.</small></div><strong>13</strong>"></div>
-                  <div style="--value:36" data-tooltip="<div><small>20 янв. 2020 г.</small></div><strong>14</strong>"></div>
-                  <div style="--value:39" data-tooltip="<div><small>21 янв. 2020 г.</small></div><strong>15</strong>"></div>
-                  <div style="--value:7" data-tooltip="<div><small>22 янв. 2020 г.</small></div><strong>3</strong>"></div>
-                  <div style="--value:34" data-tooltip="<div><small>23 янв. 2020 г.</small></div><strong>13</strong>"></div>
-                  <div style="--value:42" data-tooltip="<div><small>24 янв. 2020 г.</small></div><strong>16</strong>"></div>
-                  <div style="--value:42" data-tooltip="<div><small>25 янв. 2020 г.</small></div><strong>16</strong>"></div>
-                  <div style="--value:7" data-tooltip="<div><small>26 янв. 2020 г.</small></div><strong>3</strong>"></div>
-                  <div style="--value:31" data-tooltip="<div><small>27 янв. 2020 г.</small></div><strong>12</strong>"></div>
-                  <div style="--value:39" data-tooltip="<div><small>28 янв. 2020 г.</small></div><strong>15</strong>"></div>
-                  <div style="--value:36" data-tooltip="<div><small>29 янв. 2020 г.</small></div><strong>14</strong>"></div>
-                  <div style="--value:18" data-tooltip="<div><small>30 янв. 2020 г.</small></div><strong>7</strong>"></div>
-                  <div style="--value:15" data-tooltip="<div><small>31 янв. 2020 г.</small></div><strong>6</strong>"></div>
-                  <div style="--value:34" data-tooltip="<div><small>1 февр. 2020 г.</small></div><strong>13</strong>"></div>
-                  <div style="--value:34" data-tooltip="<div><small>2 февр. 2020 г.</small></div><strong>13</strong>"></div>
-                  <div style="--value:36" data-tooltip="<div><small>3 февр. 2020 г.</small></div><strong>14</strong>"></div>
-                  <div style="--value:5" data-tooltip="<div><small>4 февр. 2020 г.</small></div><strong>2</strong>"></div>
-                  <div style="--value:34" data-tooltip="<div><small>5 февр. 2020 г.</small></div><strong>13</strong>"></div>
-                  <div style="--value:42" data-tooltip="<div><small>6 февр. 2020 г.</small></div><strong>16</strong>"></div>
-                  <div style="--value:44" data-tooltip="<div><small>7 февр. 2020 г.</small></div><strong>17</strong>"></div>
-                  <div style="--value:5" data-tooltip="<div><small>8 февр. 2020 г.</small></div><strong>2</strong>"></div>
-                  <div style="--value:42" data-tooltip="<div><small>9 февр. 2020 г.</small></div><strong>16</strong>"></div>
-                  <div style="--value:50" data-tooltip="<div><small>10 февр. 2020 г.</small></div><strong>19</strong>"></div>
-                  <div style="--value:34" data-tooltip="<div><small>11 февр. 2020 г.</small></div><strong>13</strong>"></div>
-                  <div style="--value:15" data-tooltip="<div><small>12 февр. 2020 г.</small></div><strong>6</strong>"></div>
-                  <div style="--value:50" data-tooltip="<div><small>13 февр. 2020 г.</small></div><strong>19</strong>" class=""></div>
-                </div>
-              </div>
-            </div>`
-    
     get link(){
-        return '<a href="' + this.href + '">Link text</a>'
+        return '<a class="column-chart__link" href="' + this.href + '">View all</a>'
     }
     
-    remove() {
+    get href(){
+        return this.graphHref
+    }
+    
+    get label(){
+        return this.graphLabel
+    }
+    
+    getValue(){
+        return this.graphValue
+    }
+    
+    render() {
+        const wrapElement = document.createElement('div')
+        wrapElement.innerHTML = this.getTpl()
+        this.element = wrapElement.firstElementChild
+    }
+    
+    getTpl(){
+        return `<div class="column-chart">
+          <div class="column-chart__title">
+            ${this.label}
+            ${this.link}
+          </div>
+          <div class="column-chart__container">
+            <div class="column-chart__header">${this.getValue()}</div>
+            <div class="column-chart__chart">
+              ${this.getChartLines()}
+            </div>
+          </div>
+        </div>`
+    }
+    
+    getChartLines() {
+        if (this.data.length === 0) return '<img src="charts-skeleton.svg" />'
+    
+        const bars = []
+        const maxGraphValue = Math.max(...this.data)
+        this.data.forEach((thisValue) => {
+            const height = parseInt(this.chartHeight / maxGraphValue * thisValue, 10)
+            const percent = Math.round(thisValue / maxGraphValue * 100)
+            bars.push(`<div style="--value: ${height}" data-tooltip="${percent}%"></div>`)
+        })
+    
+        return bars.join('')
+    }
+    
+    destroy() {
         this.element.remove()
     }
     
-    update() {
-        this.data = 'new data'
+    remove(){
+        this.destroy()
+    }
+    
+    update(newData) {
+        this.data = newData
         this.render()
     }
 }
